903c5e76c1b80782bc32f3c163bb0438f98205a8
Ziad Ghanem <ziad.mahmoud02@eng-st.cu.edu.eg>
2023-03-17 22:40:23 +0200

    Added Email validation and outline for additional info.

--- signup/src/EnterEmail.js
+++ signup/src/EnterEmail.js
@@ -3,24 +3,54 @@ import { useState } from 'react';
 
 function CreateAccount() {
   const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const [showAdditionalInfo, setShowAdditionalInfo] = useState(false);
+  const [password, setPassword] = useState('');
+
+  function vaildateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+  function handleContinueButtonClick() {
+    setValidEmail(vaildateEmail(email));
+    setShowAdditionalInfo(validEmail);
+  }
+  //function that handles the input change of email
+  function handleEmailChange(event) {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setValidEmail(vaildateEmail(newEmail));
+  }
 
   return (
-    <div class="create-account-main">
-      <div class="split-container-primary">
-        <div class="split-container-content">
-          <div class="header-create-element">
-            <h1>TicketWave</h1>
+    <div className="create-account-main">
+      <div className="split-container-primary">
+        <div className="split-container-content">
+          <div className="header-create-element">
+            <h1>Ticketwave</h1>
             <h1>Create an account</h1>
           </div>
           <form>
             <div id="create-account">
-              <input type="text" id="create-ccount-first-email"></input>
-              <button id="continue-button">Continue</button>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+              <button id="continue-button" onClick={handleContinueButtonClick}>
+                Continue
+              </button>
             </div>
+            <div id="confirm-email"></div>
+            <div id="first-name"></div>
+            <div id="last-name"></div>
+            <div id="password"></div>
           </form>
         </div>
       </div>
-      <div class="split-container-secondary"></div>
+      <div className="split-container-secondary"></div>
     </div>
   );
 }
