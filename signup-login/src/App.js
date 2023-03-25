1a84f97a75b3dae22cfb658a60d419a5ecf31a46
Ziad Ghanem <ziad.mahmoud02@eng-st.cu.edu.eg>
2023-03-25 14:56:03 +0200

    Added necessary files.

--- signup-login/src/App.js
+++ signup-login/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import CreateAccount from './EnterEmail';
+import SignIn from './SignIn';
+import Home from './Landing';
 
+/**
+ * Renders the main application component.
+ * @function
+ * @returns {JSX.Element} The rendered application component.
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<CreateAccount />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- signup-login/src/index.js
+++ signup-login/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/**
+ * Renders the root component of the application.
+ * @function
+ * @returns {void}
+ */
+function render() {
+  root.render(
+    //<React.StrictMode>
+    <GoogleOAuthProvider clientId="770303914933-s45dc140ig3djblj6rs9ckg30m58is1u.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+    //</React.StrictMode>
+  );
+}
+
+render();
