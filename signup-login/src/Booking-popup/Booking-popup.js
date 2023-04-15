02c2934469898c011a9c7d8c5417aa45b474a06b
AssemSadek90 <101525092+AssemSadek90@users.noreply.github.com>
2023-04-15 22:02:55 +0200

    implemented a basic registration form

--- signup-login/src/Booking-popup/Booking-popup.js
+++ signup-login/src/Booking-popup/Booking-popup.js
@@ -1,31 +1,109 @@
-import React, {useState} from 'react';
-import "./Booking-popup.css"
+import React, { useState } from 'react';
+import './Booking-popup.css';
+import MEVS from '../EventDetails/Banner/MEVS.avif'
 
-function Popup(){
+function Popup({ closeOverlay }) {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
 
-    const [showOverlay, setShowOverlay] = useState(false);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ firstName, lastName, email })
+    });
+    const data = await response.json();
+    console.log(data); // do something with the response data
+  };
 
-    const handleButtonClick = () => {
-      setShowOverlay(true);
-    }
-  
-    const handleOverlayClose = () => {
-      setShowOverlay(false);
-    }
+  return (
+    <div>
+      <div className="overlay">
+        <div className="overlay-content">
+          {/* popup left side */}
+          <div className="popup-left">
+            <header className="popup-left-header">
+              <button
+                className="closing-button"
+                onClick={() => closeOverlay(false)}
+              >
+                X
+              </button>
 
-    return(
-        <div>
-        <button id='booking-button' onClick={handleButtonClick}>Reserve a spot</button>
-        {showOverlay ? 
-          <div className="overlay">
-            <div className="overlay-content">
-              <h2>Overlay Content</h2>
-              <button onClick={handleOverlayClose}>Close Overlay</button>
+              <div className="header-title">
+                <h2>Checkout</h2>
+              </div>
+            </header>
+            <main>
+              <div>
+                <div className="form">
+                  <form onSubmit={handleSubmit}>
+                    <div className="form-row">
+                      <div className="form-group">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                          type="text"
+                          id="firstName"
+                          value={firstName}
+                          onChange={(e) => setFirstName(e.target.value)}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                          type="text"
+                          id="lastName"
+                          value={lastName}
+                          onChange={(e) => setLastName(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="email">Email Address:</label>
+                      <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                    </div>
+                  </form>
+                </div>
+                <div className="footer">
+                  <button className='register' type='submit'>register</button>
+                </div>
+              </div>
+            </main>
+          </div>
+
+          {/* popup right side */}
+          <div className="popup-right">
+            <div className="image-and-order-summary">
+              <aside className="right-container">
+                <div className="image">
+                  <img src={MEVS} className='image'></img>
+                </div>
+                <div className="order-summary-container">
+                  <div className="order-summary">
+                    <div></div>
+                    <div></div>
+                    <hr />
+                    <div></div>
+                    <div></div>
+                  </div>
+                </div>
+              </aside>
             </div>
-          </div> : null
-        }
+          </div>
+        </div>
       </div>
-    );
-
+    </div>
+  );
 }
+
 export default Popup;
+
