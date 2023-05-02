afb6db151a3e3eaad573a419b96de796d9ee5412
Ziad Ghanem <ziad.mahmoud02@eng-st.cu.edu.eg>
2023-05-02 21:21:21 +0300

    Updated Tickets styling.

--- signup-login/src/Tickets/Tickets.js
+++ signup-login/src/Tickets/Tickets.js
@@ -1,19 +1,18 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavPage from "./components/NavPage";
-import AdmissionPage from "./components/AdmissionPage";
-import PromoMain from "./promoPage/PromoMain";
-import Sidebar from "./Sidebar/Sidebar";
+import NavPage from './components/NavPage';
+import AdmissionPage from './components/AdmissionPage';
+import PromoMain from './promoPage/PromoMain';
+import Sidebar from './Sidebar/Sidebar';
 import { useLocation } from 'react-router-dom';
-import "./Tickets.css";
-import AddAttendees from "./AddAttendees/AddAttendees";
+import './Tickets.css';
+import AddAttendees from './AddAttendees/AddAttendees';
 
 function Tickets() {
-
   const [ticketData, setTicketData] = useState([]);
   const [promoData, setPromoData] = useState([]);
-  const [capacity, setCapacity] = useState(null); 
-  const [padding, setPadding] = useState('5rem') 
+  const [capacity, setCapacity] = useState(null);
+  const [padding, setPadding] = useState('5rem');
 
   // const location = useLocation();
   // const [currentUrl, setCurrentUrl] = useState(location.pathname);
@@ -22,35 +21,28 @@ function Tickets() {
   //   setCurrentUrl(location.pathname);
   // }, [location.pathname]);
 
-function TicketHandler(e, a){
-  setTicketData(e);
-  setCapacity(a);
-  // console.log(e);
-}
-  function promoHandler(b){
-    setPromoData(b)
+  function TicketHandler(e, a) {
+    setTicketData(e);
+    setCapacity(a);
+    // console.log(e);
+  }
+  function promoHandler(b) {
+    setPromoData(b);
     // console.log(b);
   }
 
-  function submitHandler(){
-    console.log("Ticket Data",
-    ticketData
-    )
-    console.log("Promo Data",
-    promoData
-    )
-    console.log("Capacity",
-    capacity
-    )
+  function submitHandler() {
+    console.log('Ticket Data', ticketData);
+    console.log('Promo Data', promoData);
+    console.log('Capacity', capacity);
 
-
-
-
-    const data = {TicketData: ticketData, PromoData: promoData, Capacity: capacity}
+    const data = {
+      TicketData: ticketData,
+      PromoData: promoData,
+      Capacity: capacity,
+    };
     // handleSignUp(data);
   }
-
-
 
   // const handleSignUp = (data) => {
   //   const requestOptions = {
@@ -63,30 +55,88 @@ function TicketHandler(e, a){
   //     .catch((error) => console.log(error));
   // };
 
-
-  useEffect(() => {window.scrollTo(0,0)}, [])
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     // <BrowserRouter>
     <React.Fragment>
-      <Sidebar isShowing={(e) => {e && setPadding('20rem')}} />
-      <div style={{paddingLeft: padding}}>
-        <Routes>
-          <Route path='/' element={<div></div>} />
+      <div className="tickets-body">
+        <Sidebar
+          isShowing={(e) => {
+            e && setPadding('20rem');
+          }}
+        />
+        <div style={{ paddingLeft: padding }}>
+          <Routes>
+            <Route path="/" element={<div></div>} />
 
-          <Route path='/Events/Tickets/admission' element={<React.Fragment><NavPage /><AdmissionPage finalCapacity={capacity} finalData={ticketData} Ticket={TicketHandler}  finalSubmission={submitHandler}/></React.Fragment>}  />
-          <Route path='Events/Tickets/add-ons' element={<React.Fragment><NavPage /><div>Add-ons</div></React.Fragment>} />
-          <Route path='/Events/Tickets/promo-codes' element={<React.Fragment><NavPage /><PromoMain finalData={promoData} Promo={promoHandler}/></React.Fragment>} />
-          <Route path='/Events/Tickets/holds' element={<React.Fragment><NavPage /><div>Holds</div></React.Fragment>} />
-          <Route path='/Events/Tickets/event-settings' element={<React.Fragment><NavPage /><div>Settings</div></React.Fragment>} />
+            <Route
+              path="/Events/Tickets/admission"
+              element={
+                <React.Fragment>
+                  <NavPage />
+                  <AdmissionPage
+                    finalCapacity={capacity}
+                    finalData={ticketData}
+                    Ticket={TicketHandler}
+                    finalSubmission={submitHandler}
+                  />
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="Events/Tickets/add-ons"
+              element={
+                <React.Fragment>
+                  <NavPage />
+                  <div>Add-ons</div>
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="/Events/Tickets/promo-codes"
+              element={
+                <React.Fragment>
+                  <NavPage />
+                  <PromoMain finalData={promoData} Promo={promoHandler} />
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="/Events/Tickets/holds"
+              element={
+                <React.Fragment>
+                  <NavPage />
+                  <div>Holds</div>
+                </React.Fragment>
+              }
+            />
+            <Route
+              path="/Events/Tickets/event-settings"
+              element={
+                <React.Fragment>
+                  <NavPage />
+                  <div>Settings</div>
+                </React.Fragment>
+              }
+            />
 
-          <Route path='/Events/Add-Attendees' element={<React.Fragment><AddAttendees finalData={ticketData} /></React.Fragment>} />
-
-        </Routes>
+            <Route
+              path="/Events/Add-Attendees"
+              element={
+                <React.Fragment>
+                  <AddAttendees finalData={ticketData} />
+                </React.Fragment>
+              }
+            />
+          </Routes>
+        </div>
       </div>
     </React.Fragment>
-  // </BrowserRouter>
+
+    // </BrowserRouter>
   );
 }
 
