ed96d69e84bdc0f0c608d3f590aa924e9fbbdc94
HananTawfik <102436066+HananTawfik@users.noreply.github.com>
2023-05-04 19:53:32 +0300

    functional documentation Row.js

--- signup-login/src/Tickets/AddAttendees/Row.js
+++ signup-login/src/Tickets/AddAttendees/Row.js
@@ -1,44 +1,40 @@
 
 import React, { useState, useEffect } from "react";
 
-function Row({item, data, myFinalData, backgroundColor, soldTickets, initialQuantity}){
+/** Renders a row for the given item in the table
+@param {object} props - Props object containing item, data, myFinalData, backgroundColor, soldTickets, and initialQuantity
+@returns {JSX.Element} - Returns a JSX element
+*/
+    function Row({item, data, myFinalData, backgroundColor, soldTickets, initialQuantity}){
 
-
+    /**React state hook to manage the quantity of tickets for a specific item */
     const [quantity, setQuantity] = useState(0);
+    /**React state hook to manage the total cost of tickets for a specific item*/
     const [total, setTotal] = useState(0);
+    /**React state hook to manage the current item*/
     const [currentItem, setCurrentItem] = useState("");
+    /**React state hook to manage the available tickets for a specific item*/
     const [available, setAvailable] = useState(item.quantity);
 
+    /**React hook that runs when component mounts or when soldTickets prop changes to update the available tickets for a specific item*/
     useEffect(() => {
-      
-    
       const current = soldTickets.find((e) => e.id === item.id);
-    
-
       setAvailable(item.quantity - current.soldTickets)
-
       setCurrentItem(current)
-
       // console.log("Sold Tickets", current.soldTickets);
-
-
     }, [soldTickets]);
-    
 
-
-
-
-
+    /** Function to handle changing the quantity of tickets for a specific item
+@param {object} e - Event object
+*/
     function changeHandler(e){
         setQuantity(e.target.value)
         setTotal(item.price * e.target.value)
-
     }
 
+    /**React hook that runs when the quantity state changes to update the data array with the chosen quantity and total cost for a specific item*/
     useEffect(() => {
         const itemIndex = data.findIndex((d) => d.id === item.id); // find index of item with item.id
-
-
         if (itemIndex >= 0) {
           const updatedData = [...data];
           updatedData[itemIndex].chosenQuantity = +quantity;
@@ -49,11 +45,7 @@ function Row({item, data, myFinalData, backgroundColor, soldTickets, initialQuan
         
       }, [quantity]);
 
-
       
-
-
-
 
     return(
 
