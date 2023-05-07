6f44216385d8a69dda205cb867f71dcd682106c0
Ziad Ghanem <ziad.mahmoud02@eng-st.cu.edu.eg>
2023-05-08 00:22:30 +0300

    Update App.js

--- signup-login/src/App.js
+++ signup-login/src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //import './App.css';
-import CreateAccount from "./Log-in/EnterEmail";
-import SignIn from "./Log-in/SignIn";
-import Home from "./Landing-page/Landing";
-import Navbar from "./NavBar/Navbar";
-import EventDetails from "./EventDetails/EventDetailsPage";
-import CreateEventForm from "./Basic-info/CreateEventForm";
-import PublishApp from "./Publish/PublishApp";
-import Terms from "./Log-in/TermsConditions/Terms";
-import ChangePassword from "./Log-in/ChangePassword";
-import Tickets from "./Tickets/Tickets";
-import Sidebar from "./Tickets/Sidebar/Sidebar";
-import { useState } from "react";
-import CreatorEvent from "./EventsList/CreatorEvent";
+import CreateAccount from './Log-in/EnterEmail';
+import SignIn from './Log-in/SignIn';
+import Home from './Landing-page/Landing';
+import Navbar from './NavBar/Navbar';
+import EventDetails from './EventDetails/EventDetailsPage';
+import CreateEventForm from './Basic-info/CreateEventForm';
+import PublishApp from './Publish/PublishApp';
+import Terms from './Log-in/TermsConditions/Terms';
+import ChangePassword from './Log-in/ChangePassword';
+import Tickets from './Tickets/Tickets';
+import Sidebar from './Tickets/Sidebar/Sidebar';
+import { useState } from 'react';
+import CreatorEvent from './EventsList/CreatorEvent';
 /**
  * Renders the main application component.
  * @function
  * @returns {JSX.Element} The rendered application component.
  */
 function App() {
-  const [padding, setPadding] = useState("5rem");
+  const [padding, setPadding] = useState('5rem');
   const [visible, setVisible] = useState(true);
   const [showSecondSidebar, setShowSecondSidebar] = useState(false);
 
@@ -43,17 +43,9 @@ function App() {
           <Route
             path="/home"
             Component={() => {
-              setPadding("5rem");
-              setVisible(true);
-              setShowSecondSidebar(false);
               return (
                 <React.Fragment>
-                  <Sidebar showSecond={showSecondSidebar} visible={visible} />
-                  <div style={{ paddingLeft: padding }}>
-                    <div style={{ paddingLeft: "1rem", paddingRight: "1rem" }}>
-                      <Home />
-                    </div>
-                  </div>
+                  <Home />
                 </React.Fragment>
               );
             }}
@@ -62,13 +54,13 @@ function App() {
           <Route
             path="/basic-info"
             Component={() => {
-              setPadding("18rem");
+              setPadding('18rem');
               setVisible(true);
               setShowSecondSidebar(true);
               return (
                 <React.Fragment>
                   <Sidebar showSecond={showSecondSidebar} visible={visible} />
-                  <div style={{ paddingLeft: padding, paddingBottom: "1rem" }}>
+                  <div style={{ paddingLeft: padding, paddingBottom: '1rem' }}>
                     <CreateEventForm />
                   </div>
                 </React.Fragment>
@@ -79,7 +71,7 @@ function App() {
           <Route
             path="/publish"
             Component={() => {
-              setPadding("18rem");
+              setPadding('18rem');
               setVisible(true);
               setShowSecondSidebar(true);
               return (
@@ -88,9 +80,9 @@ function App() {
                   <div style={{ paddingLeft: padding }}>
                     <div
                       style={{
-                        paddingTop: "1rem",
-                        paddingRight: "1rem",
-                        paddingBottom: "1rem",
+                        paddingTop: '1rem',
+                        paddingRight: '1rem',
+                        paddingBottom: '1rem',
                       }}
                     >
                       <PublishApp />
