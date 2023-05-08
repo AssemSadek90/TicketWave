7f811a603705e9b26a7f411b39df7e63d231633c
karimelnady2000 <73885158+karimelnady2000@users.noreply.github.com>
2023-05-08 17:45:15 +0300

    categories request adjustments

--- signup-login/src/Landing-page/DisplayEvents/Categories.js
+++ signup-login/src/Landing-page/DisplayEvents/Categories.js
@@ -45,6 +45,9 @@ export default function CategoriesNav() {
         'Content-Type': 'application/json',
         // Authorization: `Bearer ${accessToken}`,
       },
+      params: {
+        category: selectedTab,
+      },
     };
     server
       .get(`/events/list/`, requestOptions)
