4da7ac49d9e95f5b18bc36347035e0ea8c91073a
karimelnady2000 <73885158+karimelnady2000@users.noreply.github.com>
2023-05-09 18:42:52 +0300

    Update EventItem.js

--- signup-login/src/Landing-page/DisplayEvents/EventItem.js
+++ signup-login/src/Landing-page/DisplayEvents/EventItem.js
@@ -18,6 +18,19 @@ import { Navigate, useNavigate } from 'react-router-dom';
  */
 export default function EventItem(props) {
   const eventLink = '/event-details/' + props.event.id;
+  const realStart = () => {
+    const date = new Date(props.event.start);
+    const monthName = date.toLocaleString('default', { month: 'long' });
+    const day = date.getDate();
+    let hour = date.getHours();
+    const amPm = hour < 12 ? 'am' : 'pm';
+    hour = hour % 12 || 12;
+    const minute = date.getMinutes();
+
+    return `${monthName} ${day} · ${hour}:${minute
+      .toString()
+      .padStart(2, '0')} ${amPm}`;
+  };
 
   return (
     <div id="event-element" className={styles.event_element}>
@@ -28,14 +41,14 @@ export default function EventItem(props) {
           width="100%"
           height="50%"
         >
-          <image href={props.event.path} width="100%" height="100%" />
+          <image href={props.event.url} width="100%" height="100%" />
         </svg>
         <div id="event-details" className={styles.event_details}>
           <p id="event-detail-1" className={styles.event_detail_1}>
-            {props.event.start}
+            {props.event.name}
           </p>
           <p id="event-detail-2" className={styles.event_details_2}>
-            {props.event.id}
+            {realStart()}
           </p>
           <p id="event-detail-3" className={styles.event_detail_3}>
             {props.event.organizer}
